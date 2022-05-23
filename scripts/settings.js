5ef41cea12646ebb6e25a037afb3b1c172e92682
Ernie R <91326168+PeeachPie@users.noreply.github.com>
2022-05-23 20:47:01 +0500

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,26 +1,27 @@
-const $questionsSettings = document.querySelector('.questions-settings');
+"use strict";
+
+const $questionsSettings = document.querySelector(".questions-settings");
 
 const $questionsNumber = document.querySelector("#q");
-const $questionsRange  = document.querySelector("#questions");
+const $questionsRange = document.querySelector("#questions");
 
-const $operatorsSettings = document.querySelector('.operators-settings');
+const $operatorsSettings = document.querySelector(".operators-settings");
 
 const $operators = document.querySelectorAll(".operators .choose > *");
-const $numbers   = document.querySelectorAll(".numbers .choose > *");
+const $numbers = document.querySelectorAll(".numbers .choose > *");
 
-const [$plus, $minus, $multiply, $division]      = $operators;
-const [$n1,$n2,$n3,$n4,$n5,$n6,$n7,$n8,$n9,$n10] = $numbers;
+const [$plus, $minus, $multiply, $division] = $operators;
+const [$n1, $n2, $n3, $n4, $n5, $n6, $n7, $n8, $n9, $n10] = $numbers;
 
-const $numbersBlock = document.querySelector('.numbers')
-const $maxBlock     = document.querySelector('.max')
+const $numbersBlock = document.querySelector(".numbers");
+const $maxBlock = document.querySelector(".max");
 
-
-const $next   = document.querySelector("#next");
-const $reset  = document.querySelector("#reset");
+const $next = document.querySelector("#next");
+const $reset = document.querySelector("#reset");
 const $return = document.querySelector("#return");
-const $start  = document.querySelector("#start");
+const $start = document.querySelector("#start");
 
-const $maxRange  = document.querySelector("#max");
+const $maxRange = document.querySelector("#max");
 const $maxNumber = document.querySelector("#m");
 
 function getRandom(min, max) {
@@ -32,36 +33,36 @@ function randomChoice(choice) {
 }
 
 let Tasks = {
-  operators: ['+'], // операторы
+  operators: ["+"], // операторы
   numbers: [1],     // множители/числители
   max: 10,          // максимальное значение для сложения/вычитания
   questions: 5,     // количество вопросов в тесте
 
   // устанавливает значение по селектору
-  setter (selector, value, button) {
-    index = this[selector].indexOf(value);
-    if ((index === -1)) {
+  setter(selector, value, button) {
+    let index = this[selector].indexOf(value);
+    if (index === -1) {
       this[selector].push(value);
-      button.className = 'selected'
-    } 
-    else if (this[selector].length > 1) {
+      button.className = "selected";
+    } else if (this[selector].length > 1) {
       this[selector].splice(index, 1);
-      button.className = 'unselected'
+      button.className = "unselected";
     }
   },
 
   // устанавливает оператор
-  setOperator : function (value, button) {
-    Tasks.setter('operators', value, button)
+  setOperator: function (value, button) {
+    Tasks.setter("operators", value, button);
   },
 
   // утанавливает делитель/множитель
-  setNumber : function (value, button) {
-    Tasks.setter('numbers', value, button)
+  setNumber: function (value, button) {
+    Tasks.setter("numbers", value, button);
   },
 
   // создает пример
-  createEquation (operator, max) {
+  createEquation(operator, max) {
+    let a, b;
     switch (operator) {
       case "+":
         a = getRandom(0, max / 2);
@@ -83,80 +84,77 @@ let Tasks = {
   },
 
   // создает примеры
-  createEquations () {
-    let problems = {}
+  createEquations() {
+    let problems = [];
     for (let i = 1; i <= this.questions; i++) {
-      equation = this.createEquation(randomChoice(this.operators), this.max);
-      problems[`problem${i}`] = {
+      let equation = this.createEquation(randomChoice(this.operators), this.max);
+      problems.push({
         eq: equation[0],
         ans: equation[1],
         given: null,
         right: false,
-      };
+      });
     }
     return problems;
-  }
+  },
 };
 
 // обрабатывает событие двойного нажатия
-function doubleClick (button) {
-  let time = new Date().getTime()
-  button.addEventListener('click', () => {
-    if (new Date().getTime() - time <= 500) {
-      selectNumbersBefore(button)
-    }
-  }, {once: true})
+function doubleClick(button) {
+  let time = new Date().getTime();
+  button.addEventListener("click",() => {
+      if (new Date().getTime() - time <= 300) {
+        selectNumbersBefore(button);
+      }
+    },
+    { once: true }
+  );
 }
 
 // выбирает делители/множители до нажатой кнопки включая ее
 function selectNumbersBefore(button) {
-  num = Number(button.textContent) - 1
+  let num = Number(button.textContent) - 1;
   for (let j = num; j >= 0; j--) {
-    if ($numbers[j].className === "unselected"){
-      Tasks.setNumber(j + 1,$numbers[j])
+    if ($numbers[j].className === "unselected") {
+      Tasks.setNumber(j + 1, $numbers[j]);
     }
   }
 }
 
 // устанавливает максимальное значение для сложения/вычитания
-function setMax () {
+function setMax() {
   $maxNumber.value = $maxRange.value;
-  changeFill(((Number($maxRange.value) - 10) / 90) * 100, $maxRange)
-  Tasks.max = Number($maxRange.value)
+  changeFill(((Number($maxRange.value) - 10) / 90) * 100, $maxRange);
+  Tasks.max = Number($maxRange.value);
 }
 
 // устанавливает значение вопросов
-function setQuestions () {
+function setQuestions() {
   $questionsNumber.value = $questionsRange.value;
-  changeFill(((Number($questionsRange.value) - 5) / 25) * 100, $questionsRange)
-  Tasks.questions = Number($questionsRange.value)
+  changeFill(((Number($questionsRange.value) - 5) / 25) * 100, $questionsRange);
+  Tasks.questions = Number($questionsRange.value);
 }
 
 // изменяет заполнение input type="range"
-function changeFill (perсent, range) {
+function changeFill(perсent, range) {
   if (perсent >= 80) {
-    perсent--
+    perсent--;
   } else if (perсent <= 12) {
-    perсent++
+    perсent++;
   }
   range.style.background = `linear-gradient(90deg, rgb(22, 255, 1) ${perсent}%, 
   #1f1f1f ${perсent}%)`;
 }
 
-
 // сбрасывает настройки
 function resetSettings() {
-  for (let $number of $numbers) {
-    $number.className = "unselected";
-  }
+  $numbers.forEach(($num) => ($num.className = "unselected"));
   $numbers[0].className = "selected";
 
-  for (let $operator of $operators) {
-    $operator.className = "unselected";
-  }
+  $operators.forEach(($operator) => ($operator.className = "unselected"));
   $operators[0].className = "selected";
 
-  Tasks.operators = ['+'];
+  Tasks.operators = ["+"];
   Tasks.numbers = [1];
   Tasks.max = 10;
   Tasks.questions = 5;
@@ -166,8 +164,10 @@ function resetSettings() {
   $questionsNumber.value = "5";
   $maxNumber.value = "10";
 
-  changeFill(0, $maxRange)
-  changeFill(0, $questionsRange)
+  changeFill(0, $maxRange);
+  changeFill(0, $questionsRange);
+
+  hideElement($numbersBlock);
 }
 
 function hideElement(element) {
@@ -179,64 +179,68 @@ function showElement(element) {
 }
 
 for (let i = 0; i < $operators.length; i++) {
-  $operators[i].addEventListener("click", Tasks.setOperator.bind(Tasks, ['+', '-', '*', '/'][i], $operators[i]))
+  $operators[i].addEventListener(
+    "click",
+    Tasks.setOperator.bind(Tasks, ["+", "-", "*", "/"][i], $operators[i])
+  );
 }
 
 for (let i = 0; i < $numbers.length; i++) {
-  $numbers[i].addEventListener("click", Tasks.setNumber.bind(Tasks, i + 1, $numbers[i]))
+  $numbers[i].addEventListener("click", Tasks.setNumber.bind(Tasks, i + 1, $numbers[i])
+  );
+  $numbers[i].addEventListener("click", doubleClick.bind(null, $numbers[i]));
 }
 
-for (let i = 0; i < $numbers.length; i++) {
-  $numbers[i].addEventListener('click', doubleClick.bind(null, $numbers[i]))
-}
-
-
-$maxNumber.addEventListener('change', () => {
-  $maxRange.value = $maxNumber.value
-  setMax()
-})
+$maxNumber.addEventListener("change", () => {
+  $maxRange.value = $maxNumber.value;
+  setMax();
+});
 
 $maxRange.addEventListener("input", setMax);
 
-$questionsNumber.addEventListener('change', () => {
+$questionsNumber.addEventListener("change", () => {
   $questionsRange.value = $questionsNumber.value;
-  setQuestions()
-})
+  setQuestions();
+});
 
 $questionsRange.addEventListener("input", setQuestions);
 
 $start.addEventListener("click", () => {
   localStorage.problems = JSON.stringify(Tasks.createEquations());
   localStorage.questions = JSON.stringify(Tasks.questions);
-  window.location.href = "../pages/test.html"
+  window.location.href = "../pages/test.html";
 });
 
-window.addEventListener('load', resetSettings)
-window.addEventListener('unload', resetSettings)
+window.addEventListener("load", resetSettings);
+window.addEventListener("unload", resetSettings);
 
-window.addEventListener('click', () => {
-  if (Tasks.operators.indexOf('+') === -1 && Tasks.operators.indexOf('-') === -1 ) {
-    hideElement($maxBlock)
+window.addEventListener("click", () => {
+  if (
+    Tasks.operators.indexOf("+") === -1 &&
+    Tasks.operators.indexOf("-") === -1
+  ) {
+    hideElement($maxBlock);
   } else {
-    showElement($maxBlock)
+    showElement($maxBlock);
   }
-  if (Tasks.operators.indexOf('*') === -1 && Tasks.operators.indexOf('/') === -1 ) {
-    hideElement($numbersBlock)
+  if (
+    Tasks.operators.indexOf("*") === -1 &&
+    Tasks.operators.indexOf("/") === -1
+  ) {
+    hideElement($numbersBlock);
   } else {
-    showElement($numbersBlock)
+    showElement($numbersBlock);
   }
-})
+});
 
-$reset.addEventListener('click', resetSettings)
+$reset.addEventListener("click", resetSettings);
 
-$next.addEventListener('click', () => {
-  hideElement($operatorsSettings)
-  showElement($questionsSettings)
-})
+$next.addEventListener("click", () => {
+  hideElement($operatorsSettings);
+  showElement($questionsSettings);
+});
 
-$return.addEventListener('click', () => {
-  hideElement($questionsSettings)
-  showElement($operatorsSettings)
-})
-
-hideElement($numbersBlock)
+$return.addEventListener("click", () => {
+  hideElement($questionsSettings);
+  showElement($operatorsSettings);
+});
