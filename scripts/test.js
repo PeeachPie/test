487241b71a2d78cd05861bedb07a136477935757
Ernie R <91326168+PeeachPie@users.noreply.github.com>
2022-05-23 20:47:52 +0500

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,23 +1,26 @@
+"use strict";
+
 const questions = JSON.parse(localStorage.questions);
 let $container = document.querySelector(".container");
-let $result    = document.querySelector(".result");
-let $task      = document.querySelector(".task");
-let $problem   = document.querySelector(".problem");
-let $answer    = document.querySelector("#answer");
-let $new       = document.querySelector(".new");
-let $check     = document.querySelector(".check");
-let $message   = document.querySelector(".message");
-let $counter   = document.querySelector(".counter");
+let $result = document.querySelector(".result");
+let $task = document.querySelector(".task");
+let $problem = document.querySelector(".problem");
+let $answer = document.querySelector("#answer");
+let $new = document.querySelector(".new");
+let $check = document.querySelector(".check");
+let $message = document.querySelector(".message");
+let $counter = document.querySelector(".counter");
 
 let counter = 0;
 
-problems = JSON.parse(localStorage.problems);
+let problems = JSON.parse(localStorage.problems);
+
 problems.checkAns = function () {
-  let problem = `problem${counter}`;
-  this[problem].given = Number.parseInt($answer.value);
+  this[counter].given = Number.parseInt($answer.value);
   $answer.value = "";
-  this[problem].right = this[problem].ans === this[problem].given;
-  return this[problem].right;
+  this[counter].right = this[counter].ans === this[counter].given;
+  counter++;
+  return this[counter - 1].right;
 };
 
 function getRandom(min, max) {
@@ -30,16 +33,15 @@ function randomChoice(choice) {
 
 // создает новое задание
 function createNewTask(problems) {
-  counter += 1;
-  if (counter > questions) {
+  if (counter + 1 > questions) {
     localStorage.problems = JSON.stringify(problems);
     window.location.href = "../pages/result.html";
   } else {
-    $counter.textContent = `${counter} задание из ${questions}`;
+    $counter.textContent = `${counter + 1} задание из ${questions}`;
     $task.style.display = "block";
     $result.style.display = "none";
     $container.style.borderColor = "#4a4a4a";
-    $problem.textContent = problems[`problem${counter}`].eq;
+    $problem.textContent = problems[counter].eq;
   }
 }
 
@@ -53,18 +55,13 @@ function change(...elements) {
 function showAns() {
   $task.style.display = "none";
   $result.style.display = "block";
-  // $counter.style.color = "#ebebeb";
-  if (problems.checkAns()) {
-    rightAns();
-  } else {
-    wrongAns();
-  }
+  problems.checkAns() ? rightAns() : wrongAns();
 }
 
 // отображается в случае правильного ответа
 function rightAns() {
   $container.style.border = "0.5vmin solid rgb(22, 255, 1)";
-  $message.style.color = 'rgb(22, 255, 1)'
+  $message.style.color = "rgb(22, 255, 1)";
   $message.textContent = randomChoice([
     "Ты молодец!",
     "Так держать!",
@@ -76,7 +73,7 @@ function rightAns() {
 // отображается в случае неправильного ответа
 function wrongAns() {
   $container.style.border = "0.5vmin solid rgb(255, 55, 55)";
-  $message.style.color = 'rgb(255, 55, 55)'
+  $message.style.color = "rgb(255, 55, 55)";
   $message.textContent = randomChoice([
     "Ой...",
     "Ошибка!",
@@ -90,3 +87,4 @@ $answer.addEventListener("change", showAns);
 $new.addEventListener("click", createNewTask.bind(null, problems));
 
 createNewTask(problems);
+
