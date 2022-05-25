3cfc4c67c9c18de19f575e40564000e9dc3a0f74
Ernie R <91326168+PeeachPie@users.noreply.github.com>
2022-05-25 18:51:46 +0500

    Delete utilits.js

--- utilits.js
+++
@@ -1,7 +0,0 @@
-export function getRandom(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
-
-export function randomChoice(choice) {
-  return choice[getRandom(0, choice.length - 1)];
-}
