2452541912c1a5bae89f8a51fc4ffce350926fa5
Ernie R <91326168+PeeachPie@users.noreply.github.com>
2022-05-25 18:51:05 +0500

    Add files via upload

---
+++ utilits.js
@@ -0,0 +1,7 @@
+export function getRandom(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+export function randomChoice(choice) {
+  return choice[getRandom(0, choice.length - 1)];
+}
